7c514d9ba6209a89b7b1e1a04c742e3d81176bb4
Emin Durak <emin@pomegra.org>
2017-12-07 15:22:06 +0100

    saving the username instead of the id

--- server/methods/methods.js
+++ server/methods/methods.js
@@ -4,7 +4,7 @@ Meteor.methods({
 	addNode(node, userId) {
 		if (Meteor.userId() === userId) {
 			try {
-				node.guide = Meteor.userId();
+				node.guide = Meteor.user().username;
 				Needs.insert(node);
 			} catch(error) {
 				return error;
